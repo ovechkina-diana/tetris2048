f3b6697d696bd0d0b1c2e4f61b33579896d710f3
tatgricenko <tattgricenko@gmail.com>
2022-10-03 22:34:11 +0300

    Разделение Model на классы

--- Current.cs
+++ Current.cs
@@ -10,9 +10,10 @@ namespace tetris2048
     {
         static Random random = new Random();
 
-        public int x { set; get; }
-        public int y { set; get; }
-        public int value { set; get; }
+        public int x { get; set; }
+        public int y { get; set; }
+        public int value { get; set; }
+        public bool IsFell { get; set; } // если EndCurrent == true значит текущий кубик упал
 
         public Current(int MaxDegree, int x = 2, int y = 0)
         {
@@ -20,6 +21,7 @@ namespace tetris2048
             this.y = y;
             int Degree = random.Next(1, MaxDegree); // выбираем рандомную степень
             value = (int)Math.Pow(2, Degree);
+            IsFell = false;
         }
     }
 }
--- Map.cs
+++ Map.cs
@@ -8,43 +8,45 @@ namespace tetris2048
 {
     class Map
     {
-        private const int width = 5;
-        public int GetWidth() { return width; }
+        public const int width = 5;
+        public const int length = 8;
 
-        private const int length = 8;
-        public int GetLength() { return length; }
+        private static int[,] map = new int[width, length];
 
-        private int[,] map;
-
-        public Map()
-        {
-            map = new int[width, length];
-        }
-
-        public int Get(int x, int y)
+        public static int Get(int x, int y)
         {
             if (OnMap(x, y))
                 return map[x, y];
             return -1;
         }
 
-        public void Set(int x, int y, int number)
+        public static void Set(int x, int y, int number)
         {
             if (OnMap(x, y))
                 map[x, y] = number;
         }
 
-        private bool OnMap(int x, int y)
+        private static bool OnMap(int x, int y)
         {
             return x >= 0 && x < width &&
                    y >= 0 && y < length;
         }
 
-        public void Show()
+        public static void Show()
         {
-            for (int i = 0; i < width; i++)
-                for (int j = 0; j < length; j++)
-                    Console.Write(map[i, j]);
+            for (int y = 0; y < length; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    Console.SetCursorPosition(x * 5 + 5, y * 2 + 2);
+
+                    int number = map[x, y];
+
+                    Console.Write(number == 0 ? " . " : number.ToString() + "  ");
+                }
+                if (y == 1)
+                    Console.Write("-------");
+            }
         }
     }
 }
